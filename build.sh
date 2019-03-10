31c9e308c1e0fef12b582f49be8ea25920af6bf0
Thomas J. Webb <thomas@osakared.com>
2019-03-10 11:33:42 -0700

    Updated build script

--- build.sh
+++ build.sh
@@ -5,4 +5,6 @@ npx lix install gitlab:haxe-grig/grig.synth
 npx lix download
 npx haxe doc.hxml
 rm -rf dist/api
-lix run dox -i bin -o dist/api
+npx lix run dox -i bin -o dist/api
+npx lix download
+npx haxe web.hxml
