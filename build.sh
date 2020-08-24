12ad33af0b3aa9bc358831b8690d44cd9583f63f
Thomas J. Webb <thomas@osakared.com>
2020-08-23 19:50:15 -0700

    Switched to manually updating grig libs

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
 NODE_ENV=production npx webpack --progress --hide-modules --config=node_modules/laravel-mix/setup/webpack.config.js && npx @11ty/eleventy --config=.eleventy.config.js
-npx lix install gitlab:haxe-grig/grig.midi
-npx lix install gitlab:haxe-grig/grig.pitch
-npx lix install gitlab:haxe-grig/grig.synth
-npx lix install gitlab:haxe-grig/grig.audio
-npx lix install gitlab:haxe-grig/haxe-musicpd
 npx lix download
 npx haxe doc.hxml
 rm -rf dist/api
