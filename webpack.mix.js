1422ad0f388ae6c27f63117593d96e6972cbda31
Thomas J. Webb <thomas@osakared.com>
2020-08-23 20:36:14 -0700

    Turning off purge and html-minify since they both cause problems

--- webpack.mix.js
+++ webpack.mix.js
@@ -48,39 +48,39 @@ mix
         // Remove any unused CSS using Purge
         mix
 
-            .purgeCss({
-                folders: [
-                    'site'
-                ],
-                extensions: [
-                    'html',
-                    'njk'
-                ],
-                whitelist: [
-                    'body',
-                    'html',
-                    'a',
-                    'h1',
-                    'h2',
-                    'h3',
-                    'h4',
-                    'p',
-                    'blockquote',
-                    'breadcrumbs',
-                    'content',
-                    'form',
-                    'input',
-                    'textarea',
-                    'intro',
-                    'btn',
-                    'loaded',
-                    'page-title',
-                    'required',
-                    'row',
-                    'visually-hidden',
-                    'menu-visible'
-                ]
-            })
+            // .purgeCss({
+            //     folders: [
+            //         'input'
+            //     ],
+            //     extensions: [
+            //         'html',
+            //         'njk'
+            //     ],
+            //     whitelist: [
+            //         'body',
+            //         'html',
+            //         'a',
+            //         'h1',
+            //         'h2',
+            //         'h3',
+            //         'h4',
+            //         'p',
+            //         'blockquote',
+            //         'breadcrumbs',
+            //         'content',
+            //         'form',
+            //         'input',
+            //         'textarea',
+            //         'intro',
+            //         'btn',
+            //         'loaded',
+            //         'page-title',
+            //         'required',
+            //         'row',
+            //         'visually-hidden',
+            //         'menu-visible'
+            //     ]
+            // })
 
             // Minifies CSS & JS files
             .minify(paths.sass.dest + 'main.css')
